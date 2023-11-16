13aa014412920571721521f1f33810fa3dd4f98b
Yann Erbs <yerbs@k2r4p4.42mulhouse.fr>
2023-11-16 12:04:53 +0100

    %c et %s qui marchent

---
+++ include/ft_printf.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yerbs <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/14 09:37:19 by yerbs             #+#    #+#             */
+/*   Updated: 2023/11/16 11:48:46 by yerbs            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdio.h>
+
+int	ft_printf(const char *str, ...);
+void    ft_formats(va_list args, const char format);
+void	ft_putchar(char c);
+void    ft_putstr(char *str);
+
+#endif
